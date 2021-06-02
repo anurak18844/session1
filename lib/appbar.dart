827df3b1014d4771c571c662f9d356195508d678
anurak18844 <anurak18844@gmail.com>
2021-06-02 12:55:49 +0700

    Re Commit

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -21,60 +21,84 @@ class _MyHomepageState extends State<MyHomepage> {
   int number = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("CALCUlATE"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.comment),
-            tooltip: "Comment Icon",
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("CALCUlATE"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.comment),
+              tooltip: "Comment Icon",
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.shopping_cart),
+              tooltip: "Shopping Cart",
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.settings),
+              tooltip: "Setting Icon",
+              onPressed: () {},
+            ),
+          ],
+          titleSpacing: 00.0,
+          centerTitle: true,
+          // toolbarHeight: 70.2,
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: "Menu Icon",
             onPressed: () {},
           ),
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            tooltip: "Shopping Cart",
-            onPressed: () {},
+          brightness: Brightness.dark,
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.directions_car)),
+              Tab(icon: Icon(Icons.directions_transit)),
+            ],
           ),
-          IconButton(
-            icon: Icon(Icons.settings),
-            tooltip: "Setting Icon",
-            onPressed: () {},
-          ),
-        ],
-        titleSpacing: 00.0,
-        centerTitle: true,
-        toolbarHeight: 70.2,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: "Menu Icon",
-          onPressed: () {},
         ),
-        brightness: Brightness.dark,
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "CLICK ME NOW!",
-            style: TextStyle(fontSize: 24),
-          ),
-          Text(
-            number.toString(),
-            style: TextStyle(fontSize: 60),
-          ),
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          addNumber();
-        },
-        child: Icon(Icons.add),
+        body: TabBarView(
+          children: [
+            loadCounter(),
+            Icon(Icons.directions_transit),
+          ],
+        ),
+
       ),
     );
   }
 
-  void addNumber(){
+  Scaffold loadCounter() {
+    return Scaffold(
+            body: Container(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "CLICK ME NOW!",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    Text(
+                      number.toString(),
+                      style: TextStyle(fontSize: 60),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: ()  {
+                addNumber();
+              },
+              child: Icon(Icons.add),
+            ),
+          );
+  }
+
+  void addNumber() {
     setState(() {
       number++;
     });
